37bed536512b3a4ff073b1a2ce1ebd456edbda4b
c00z <zcusimano@gmail.com>
2016-12-01 17:50:24 -0800

    Click Solution

--- starter-code/change/base.js
+++ starter-code/change/base.js
@@ -4,4 +4,17 @@ $(document).ready(function(){
 
   $("#time").text( Date.now() );
 
-})
+  $(".add").change(function () {
+    var $left = $('#left').val();
+        //console.log($left);
+    var $right = $('#right').val();
+    var total = Number($right) + Number($left);
+    $("#total").val(total);
+  })
+});
+
+
+
+// <input type="number" class="add" id="left"> +
+// <input type="number" class="add" id="right"> =
+// <input type="number" id="total"><br>
--- starter-code/click/base.js
+++ starter-code/click/base.js
@@ -2,4 +2,15 @@ console.log("Sanity Check: JS is working!");
 
 $(document).ready(function(){
 
+  var time = ( Date.now() );
+
+  $('span').on('click', function() {
+    var newParam = $(this).text();
+    $('ul').append('<li></li>' + "You clicked: "+ newParam + " at " + time );
+
+  })
+
 })
+
+
+//Create a list of all the phrases that are clicked.
