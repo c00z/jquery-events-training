fe7b5ca223423555d99ddff0aac697ad6bfb6a11
c00z <zcusimano@gmail.com>
2016-12-02 13:14:25 -0800

    All solutions aside forom keypress

--- starter-code/keypress/base.js
+++ starter-code/keypress/base.js
@@ -1,5 +1,15 @@
 console.log("Sanity Check: JS is working!");
 
 $(document).ready(function(){
+var startTime = ( Date.now());
+var endtime = "";
+  .keypress() {
+    if(e = 32) {
+      
+
+      }
+
+    }
+  }
 
 })
